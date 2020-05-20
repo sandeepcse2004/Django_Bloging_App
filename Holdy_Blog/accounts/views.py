02494a6f07340f0c652de490462f83c94d91c5fd
sandeepcse2004 <SkumTiwari@ad.harman.com>
2020-05-20 16:12:02 +0530

    some refactoring

--- Holdy_Blog/accounts/views.py
+++ Holdy_Blog/accounts/views.py
@@ -1,34 +1,31 @@
 from django.shortcuts import render, redirect
-from . import views
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
 # Create your views here.
+
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect(request, 'articles/articles_display.html')
             return redirect('accounts:login')
-            # form1 = AuthenticationForm()
-            # return render(request, 'accounts/login.html', {'form1' : form1})
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form' : form})
+    return render(request, 'accounts/signup.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
         form1 = AuthenticationForm(data=request.POST)
         if form1.is_valid():
-            user =  form1.get_user()
-            login(request,user)
+            user = form1.get_user()
+            login(request, user)
             return redirect('articles:create')
     else:
         form1 = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form1' : form1})
+    return render(request, 'accounts/login.html', {'form1': form1})
 
 
 def logout_view(request):
--- Holdy_Blog/articles/views.py
+++ Holdy_Blog/articles/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Article
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from . import forms
+
 
 # Create your views here.
 def home(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, 'articles/articles_display.html', {'articles' : articles})
+    return render(request, 'articles/articles_display.html', {'articles': articles})
+
 
 def profile(request):
     return HttpResponse('Hello profile')
 
+
 def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
-    return render(request, 'articles/article_detail.html', {'article' : article})
+    return render(request, 'articles/article_detail.html', {'article': article})
 
 
 def article_create(request):
@@ -28,4 +30,4 @@ def article_create(request):
             return redirect('articles:home')
     else:
         form = forms.CreateArticle()
-    return render(request, 'articles/article_create.html', {'form' : form})
+    return render(request, 'articles/article_create.html', {'form': form})
